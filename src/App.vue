61093ba4f2a9f010c1f16bcbe712bbbc4c06ef29
unknown <931744496@qq.com>
2019-06-05 21:40:37 +0800

    KEYUTIL.getKey

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>JWT</h1>
+    <div class="jwt">{{JWT}}</div>
   </div>
 </template>
 
@@ -42,11 +43,16 @@ export default {
     setJWT() {
       let header = JSON.stringify(this.jwt_header);
       let payload = JSON.stringify(this.jwt_payLoad);
+
+      // let secret = 'MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBANESTe7LjH2LhrXo5g5gSnkivZ/XqWyZQcHYeMYOXGRTSO71gCnJ5mVRdvX3VmTEna/Hb68qmk3iAosPLmvskxOnByHUI29x7JJfoOIziXBMCdQRmIFiA0E2sog0S0mZdZJkFN5Hu/scf8TE0/m/KGGTTovWU6iSeFhyr30WNMaHAgMBAAECgYAE609WHQfzNEM5KH+xOubFruGTTzm3SmvXqcY9srzNx3/hz3jygsOfAqmv49/ugwnKxwCDuJbk2jqBFxxagbh8JPmMG8nNwyWhmcDfqwTRu3OVOP//vXigFiIRw7hpYLskHIyBVZnZZqSotAaiK/igp3OLOBzQuYILdczn4X5GkQJBAPjIdAm4l3r+OBcgTy/zcCJQkOHQ3yc63ZIapbNmlgozU5RAhX6yzdKRHPnJap9QEqDJTw5WRFaPvxG5mLZayKMCQQDXIvCj6muQGbmuBtAgZGcMnkFt//T+n59Il+ba2JDWj3hOO6Emzj16EUu0kqlBP3fGU6cXAjusAlZVR0s9SzTNAkAvjqIf+Zl7eX1fbl203ORiquQHRtZhuW8BrvZeBQ5JhOZFQNBEGAogZn0Tgt1O9w+YjOL/6p3FrlToHoKC2XfhAkEAipTPPkd7Ek//88Ifvz3tw4sNyrXeM0bPbAutgbuPUScJ8BspK74ei8soYSE4NfeUSAUK1R9zINJAmp5aMRmI4QJBAL29qAbT53Eua+VeDqxgQ3Vz54jwokhqkqDRcHM8Cphx9PZyEXd1Q8DehN+uTnUxsdoaMjVSR3vpWnZDzckKVuk=';
+
+      // let prvKey = jsrsasign.KEYUTIL.getKey('sPKCS8PEM', this.number);
+      // console.log(prvKey);
       this.JWT = jsrsasign.KJUR.jws.JWS.sign(
         "HS256",
         header,
         payload,
-        this.number
+        this.number,
       );
     }
   }
@@ -61,5 +67,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.jwt{
+  width: 500px;
+  min-height: 500px;
+  text-align: left;
+  margin: 0 auto;
+  white-space: normal;
+  word-break: break-all;
 }
 </style>
