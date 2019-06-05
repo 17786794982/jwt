5c4901c7c24f53c3351413513494aba504b153b2
seven <931744496@qq.com>
2019-06-05 18:28:46 +0800

    jwt

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,61 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>JWT</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import jsrsasign from "jsrsasign";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      number: "123",
+
+      jwt_header: {
+        alg: "HS256",
+        typ: "JWT"
+      },
+      jwt_payLoad: {},
+      JWT: ""
+    };
+  },
+  mounted() {
+    this.$nextTick(function() {
+      this.setPayLoad();
+      this.setJWT();
+    });
+  },
+  methods: {
+    /** 设置负载 */
+    setPayLoad() {
+      var tNow = new Date().getTime();
+      var tEnd = tNow + 1000 * 60 * 60;
+      this.jwt_payLoad = {
+        sub: "forget",
+        iat: tNow, // 签发时间
+        nbf: tNow, // 生效时间
+        exp: tEnd // 过期时间
+      };
+    },
+    setJWT() {
+      let header = JSON.stringify(this.jwt_header);
+      let payload = JSON.stringify(this.jwt_payLoad);
+      this.JWT = jsrsasign.KJUR.jws.JWS.sign(
+        "HS256",
+        header,
+        payload,
+        this.number
+      );
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
